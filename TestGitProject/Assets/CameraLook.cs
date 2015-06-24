d3b9914d672ad6e1eaf8f6ddffe5793b07baa9b7
Oscar Louw <ojlou1@student.monash.edu>
2015-06-24 13:36:36 +1000

    Test push
    pls work

---
+++ TestGitProject/Assets/CameraLook.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLook : MonoBehaviour {
+    public Transform ball;
+
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(ball);
+	}
+}
